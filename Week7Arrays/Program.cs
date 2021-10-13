123d04727e58f854bcbd9635e66a3efd5d360593
Julia Aasmaa <my_e_10@hotmail.com>
2021-10-13 10:44:41 +0300

    massiivid

---
+++ Week7Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Week7Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm palub kasutajal valida, mida ta hommikusöögiks sööb
+
+            string[] food = { "võileib juustuga", "jogurt", "banaan", "peekon", "muna" };
+
+            food[3] = "viinerid";
+            food[1] = "keefir";
+
+            Console.WriteLine("Menüüs:");
+
+
+            for (int i = 0; i < food.Length; i++)
+            {
+
+                Console.WriteLine(food[i]);
+            }
+
+            Console.WriteLine("Vali toit (sisesta number 1 - 5:");
+            int userChoice = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            Console.WriteLine($"Oled valinud {food[userChoice]} ");
+
+        }
+    }
+}
